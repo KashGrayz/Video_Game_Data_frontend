f451652b122955eb16aab842cb850358db2f8811
StevenSisavath <stevenlsisavath@gmail.com>
2023-01-11 17:02:08 -0600

    Updated GlobalSalesChart Component Functionalites

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import GlobalSalesChart from './Components/GlobalSalesChart/GlobalSalesChart';
+import axios from 'axios';
 
 
 function App() {
+  const [games, setGames] = useState([]);
 
+  async function getGames(){
+      let response = await axios.get('http://localhost:8080/all');
+      setGames(response.data);
+  }
+
+  useEffect(()=>{
+    getGames();
+  }, [])
 
   return (
     <div>
-      <GlobalSalesChart/>
+      <GlobalSalesChart games={games}/>
     </div>
   );
 }
--- src/Components/GlobalSalesChart/GlobalSalesChart.jsx
+++ src/Components/GlobalSalesChart/GlobalSalesChart.jsx
@@ -1,32 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
+import {Chart} from 'react-google-charts';
 
-const GlobalSalesChart = (props) => {
-    const [games, setGames] = useState([]);
+const GlobalSalesChart = ({games}) => {
 
-    async function getGames(){
-      let response = await axios.get('http://localhost:8080/all');
-      setGames(response.data);
-      console.log(response.data)
-      filterGlobalSales(response.data)
-    }
+    //Filter all games 2013 and forward
+    let filteredGames = games.filter((game) => game.year >= 2013);
+    console.log('Filtered Games', filteredGames)
 
-    function filterGlobalSales (games){
-        let globalSaleGames = games.filter(function(el){
-            if (el.year >= 2013){
-                return true;
-            }
-        });
-        console.log(globalSaleGames)
-        return globalSaleGames
-    }
 
-    useEffect(()=>{
-        getGames();
-    }, [])
+    //Map all platforms from FilteredGames List
+    let platforms = filteredGames.map((game) => game.platform)
+    console.log('Platforms', platforms)
+
+
+    //Find all distinct platforms from Mapped platforms
+    let distinctPlatforms = [...new Set(platforms)]
+    console.log('Distinct Platforms', distinctPlatforms)
+
+
+    //Extra data arrays for Main data
+    let platformArrays = distinctPlatforms.map(platform => [platform,10,'silver'])
+    console.log('PlatformArrays', platformArrays)
+
+
+    //Data used for "Global Sales by Millions" chart
+    const data = [
+        ["Element", "Density", { role: "style" }],
+        ...(platformArrays)
+      ];
+
 
     return ( 
         <div>
+            <h1>Global Sales by Millions</h1>
+            <Chart chartType="ColumnChart" width="100%" height="400px" data={data} />
         </div>
      );
 }
