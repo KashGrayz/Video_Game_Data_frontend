6a82a5f699d95324d58125b3673d182964a1ca30
Kash Grays <kasharsgrays93@.com>
2023-01-12 10:31:28 -0500

    GlobalSalesGames Components Created

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import './App.css';
-import React, { useState } from 'react';
-import GlobalSalesChart from './Components/GlobalSalesChart/GlobalSalesChart';
+import React, { useEffect, useState } from 'react';
+import GlobalSalesGames from './Components/GlobalSalesChart/GlobalSalesChart';
+import axios from 'axios';
 
 
 function App() {
 
+  const [games, setGames] = useState([]);
 
+    useEffect(()=>{
+        getGames();
+    }, [])
+
+    async function getGames(){
+      try{
+        const response = await axios.get('http://localhost:8080/all');
+        console.log(response.data)
+        setGames(response.data);
+      } catch(ex){
+        console.log(`ERROR in getGames EXCEPTION: ${ex}`)
+
+      }
+    }
+
+    
   return (
-    <div>
-      <GlobalSalesChart/>
-    </div>
-  );
+      <div>
+        <GlobalSalesGames games={games}/>
+      </div>
+    );
+
+
 }
+
+  
+
 
 export default App;
--- src/Components/GlobalSalesChart/GlobalSalesChart.jsx
+++ src/Components/GlobalSalesChart/GlobalSalesChart.jsx
@@ -1,34 +1,53 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import { Chart } from "react-google-charts";
 
-const GlobalSalesChart = (props) => {
-    const [games, setGames] = useState([]);
 
-    async function getGames(){
-      let response = await axios.get('http://localhost:8080/all');
-      setGames(response.data);
-      console.log(response.data)
-      filterGlobalSales(response.data)
-    }
+const GlobalSalesGames = ({games}) => {
 
-    function filterGlobalSales (games){
-        let globalSaleGames = games.filter(function(el){
-            if (el.year >= 2013){
-                return true;
-            }
+    function findChartData(){
+
+        console.log(games)
+
+        let filteredGames = games.filter(game => game.year >= 2013);
+
+        console.log("Filtered Games", filteredGames )
+
+        let platforms = filteredGames.map(game => {
+            return game.platform
         });
-        console.log(globalSaleGames)
-        return globalSaleGames
+
+        console.log('Platforms', platforms)
+
+        let distinctPlatforms = [...new Set(platforms)]
+
+        console.log('Distinct Platforms', distinctPlatforms)
+
+        let platformArrays = distinctPlatforms.map(platform => {
+
+            let allGamesForPlatform = filteredGames.filter(game => game.platform == platform);
+            
+
+            return [platform, 10, "silver"]
+        });
+
+        console.log('Platform Arrays', platformArrays )
+
+        const data = [
+        ["Platform", "Sales", { role: "style" }],
+        ...platformArrays
+        ];
+
+        console.log('Data', data)
+
+          return data;
+
     }
-
-    useEffect(()=>{
-        getGames();
-    }, [])
-
+    
     return ( 
         <div>
+            <h1>Platform By Global Sales in Millions</h1>
+            <Chart chartType="ColumnChart" width="100%" height="400px" data={findChartData()} />
         </div>
      );
 }
- 
-export default GlobalSalesChart;
+export default GlobalSalesGames;
